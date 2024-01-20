0c7e0494130f4595795324ddfa579bb9a5dd8f95
SuprunArtem <temasuprun86@gmail.com>
2024-01-20 14:40:05 +0200

    make build

---
+++ index.8bd313aa.js
@@ -0,0 +1,2 @@
+document.getElementById("form").onsubmit=function(){return document.getElementById("form").reset(),!1},window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});
+//# sourceMappingURL=index.8bd313aa.js.map
